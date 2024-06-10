67196493cfc56de894c69071cda02b5570d2c272
Nico Kunz <nicokunznk00@gmail.com>
2024-06-10 02:04:58 +0200

    Improve PDF data extraction logic

--- main.py
+++ main.py
@@ -1,14 +1,45 @@
-# importing required classes 
-from pypdf import PdfReader 
+import re
+from pypdf import PdfReader
 
-# creating a pdf reader object 
-reader = PdfReader('example.pdf') 
 
-# printing number of pages in pdf file 
-print(len(reader.pages)) 
+def find_start(lines):
+    '''Find the line where the actual receipt data starts'''
+    for i, line in enumerate(lines):
+        if re.match(r" *EUR$", line):
+            return i
+    return -1
 
-# creating a page object 
-page = reader.pages[0] 
+def find_end(lines):
+    '''Find the line where the actual receipt data ends'''
+    for i, line in enumerate(lines):
+        if re.match(r" *-+", line):
+            return i
+    return -1
 
-# extracting text from page 
-print(page.extract_text()) 
+
+def main():
+    '''Main function to extract data from a pdf file'''
+    # creating a pdf reader object
+    reader = PdfReader('pdfs/REWE-eBon.pdf')
+
+    # printing number of pages in pdf file
+    print(len(reader.pages))
+
+    # creating a page object
+    page = reader.pages[0]
+
+    # extracting text from page
+    #print(page.extract_text())
+    text = page.extract_text()
+
+    lines = text.split("\n")
+    #print(text.split("\n"))
+
+    start_index = find_start(lines)
+    end_index = find_end(lines)
+
+    for j in range(start_index+1, end_index):
+        print(re.split(r"  +", lines[j]))
+
+if __name__ == "__main__":
+    main()
