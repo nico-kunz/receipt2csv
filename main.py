41c4493145a192e8b78b18c21c37d8de754b3941
Nico Kunz <nicokunznk00@gmail.com>
2024-09-17 00:59:48 +0200

    Apply autoformat

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ PERSON2 = "Name2"
 
 
 def find_start(lines):
-    '''Find the line where the actual receipt data starts'''
+    """Find the line where the actual receipt data starts"""
     for i, line in enumerate(lines):
         if re.search(r"\d+,\d{2}", line):
             return i
@@ -17,7 +17,7 @@ def find_start(lines):
 
 
 def find_end(lines):
-    '''Find the line where the actual receipt data ends'''
+    """Find the line where the actual receipt data ends"""
     for i, line in enumerate(lines):
         if re.search(r" *--+", line) or re.search(r" *SUMME", line):
             return i
@@ -25,21 +25,20 @@ def find_end(lines):
 
 
 def export_to_csv(file_name, data):
-    '''Export receipt data to csv file'''
-    with open("output/" + file_name, mode='w', encoding="utf-8") as csvfile:
-        writer = csv.writer(csvfile, delimiter=',',
-                            quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    """Export receipt data to csv file"""
+    with open("output/" + file_name, mode="w", encoding="utf-8") as csvfile:
+        writer = csv.writer(
+            csvfile, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
+        )
         writer.writerow(["Product", "Price", "Together", PERSON1, PERSON2])
         writer.writerows(data)
         writer.writerow(["", "", "", "", "", "Together", "=SUM(C:C)"])
-        writer.writerow(["", "", "", "", "", PERSON1,
-                        "=SUM(D:D) + SUM(C:C)/2"])
-        writer.writerow(["", "", "", "", "", PERSON2,
-                        "=SUM(E:E) + SUM(C:C)/2"])
+        writer.writerow(["", "", "", "", "", PERSON1, "=SUM(D:D) + SUM(C:C)/2"])
+        writer.writerow(["", "", "", "", "", PERSON2, "=SUM(E:E) + SUM(C:C)/2"])
 
 
 def extract_from_file(file):
-    '''Extract data from a pdf file'''
+    """Extract data from a pdf file"""
     # creating a pdf reader object
     reader = PdfReader(file)
     print(len(reader.pages))
@@ -64,35 +63,37 @@ def extract_from_file(file):
         split_lines += [re.split(r"  +", lines[j])]
 
     # remove items that detail amount of product
-    for (i, line) in enumerate(split_lines):
-
-        if line[0] == '':
-            split_lines[i-1][0] = split_lines[i][1] + \
-                " " + split_lines[i-1][0]
+    for i, line in enumerate(split_lines):
+        if line[0] == "":
+            split_lines[i - 1][0] = split_lines[i][1] + " " + split_lines[i - 1][0]
             print(split_lines.pop(i))
 
     # remove lines that contain "PFAND"
-    lines_without_pfand = list(
-        filter(lambda x: "PFAND" not in x[0], split_lines))
+    lines_without_pfand = list(filter(lambda x: "PFAND" not in x[0], split_lines))
 
     # filter out letters at the end of price
-    for (i, line) in enumerate(lines_without_pfand):
+    for i, line in enumerate(lines_without_pfand):
         if re.match(r"\d+,\d{2}", line[1]):
             lines_without_pfand[i][1] = re.sub(r"[a-zA-Z]|\s", "", line[1])
 
     print(lines_without_pfand)
-    export_to_csv(str.split(str.split(file, '/')
-                  [-1], '.')[0] + ".csv", lines_without_pfand)
+    export_to_csv(
+        str.split(str.split(file, "/")[-1], ".")[0] + ".csv", lines_without_pfand
+    )
 
 
 def main():
-    '''Main function, extracts data from all files in the pdfs/ folder'''
+    """Main function, extracts data from all files in the pdfs/ folder"""
 
-    argparser = argparse.ArgumentParser(description="Extract receipt data from pdf files")
-    argparser.add_argument("-f", "--file", help="Extract data from a file or folder", required=True)
+    argparser = argparse.ArgumentParser(
+        description="Extract receipt data from pdf files"
+    )
+    argparser.add_argument(
+        "-f", "--file", help="Extract data from a file or folder", required=True
+    )
     args = argparser.parse_args()
-    
-    if path.isdir(args.file):   
+
+    if path.isdir(args.file):
         files = listdir(args.file)
     else:
         files = [args.file]
