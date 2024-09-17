2269cc018f24a581f81c078de9d6c8710671bd8f
Nico Kunz <nicokunznk00@gmail.com>
2024-09-17 23:17:59 +0200

    Apply autoformat

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ import argparse
 PERSON1 = "Name1"
 PERSON2 = "Name2"
 
+
 def debug_print(*args):
     if verbose:
         print(*args)
+
 
 def find_start(lines):
     """Find the line where the actual receipt data starts"""
@@ -39,16 +41,18 @@ def export_to_csv(file_name, data):
         writer.writerow(["", "", "", "", "", PERSON1, "=SUM(D:D) + SUM(C:C)/2"])
         writer.writerow(["", "", "", "", "", PERSON2, "=SUM(E:E) + SUM(C:C)/2"])
 
+
 def extract_from_line(line: str):
     """Extract product and price from a line"""
     matches = re.findall(r"-?\d+,\d{2}", line)
     if len(matches) == 0:
         return "", ""
-    
+
     # only use price match at the end of the line
     price = matches[-1]
     product = line.split(price)[0].strip()
     return product, price
+
 
 def extract_from_file(file):
     """Extract data from a pdf file"""
@@ -79,13 +83,19 @@ def extract_from_file(file):
     for i, line in enumerate(split_lines):
         match = re.match("\d\s+(?:Stk)?\s+x", line[0])
         if match:
-            split_lines[i - 1][0] = split_lines[i][0].split("x")[0] + "x " + split_lines[i - 1][0]
+            split_lines[i - 1][0] = (
+                split_lines[i][0].split("x")[0] + "x " + split_lines[i - 1][0]
+            )
             removed = split_lines.pop(i)
             debug_print(removed)
 
     # remove lines that contain "PFAND"
     pfand_keywords = ["PFAND", "LEERGUT"]
-    lines_without_pfand = [line for line in split_lines if not any(keyword in line[0] for keyword in pfand_keywords)]
+    lines_without_pfand = [
+        line
+        for line in split_lines
+        if not any(keyword in line[0] for keyword in pfand_keywords)
+    ]
 
     debug_print(lines_without_pfand)
     export_to_csv(
@@ -105,7 +115,13 @@ def main():
 
     argparser.add_argument("-p", "--persons", help="Names of the persons", nargs=2)
 
-    argparser.add_argument("-v", "--verbose", help="Increase output verbosity", action="store_true", default=False)
+    argparser.add_argument(
+        "-v",
+        "--verbose",
+        help="Increase output verbosity",
+        action="store_true",
+        default=False,
+    )
 
     args = argparser.parse_args()
 
@@ -114,12 +130,13 @@ def main():
     if args.persons:
         PERSON1, PERSON2 = args.persons
 
-    
     global verbose
     verbose = args.verbose
 
     if path.isdir(args.file):
-        files = [ path.join(args.file, f) for f in listdir(args.file) if f.endswith(".pdf") ]
+        files = [
+            path.join(args.file, f) for f in listdir(args.file) if f.endswith(".pdf")
+        ]
     else:
         files = [args.file]
 
@@ -127,6 +144,7 @@ def main():
     makedirs("output", exist_ok=True)
 
     for file in files:
+        print("Extracting data from file: " + file)
         extract_from_file(file)
 
 
